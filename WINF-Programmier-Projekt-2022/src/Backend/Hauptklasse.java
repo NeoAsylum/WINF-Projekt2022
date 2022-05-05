82803ba2474efdb49470d894f611a7399ae235a1
adrian <adrian@adrian-MacBookAir>
2022-05-05 04:49:11 -0400

    Hauptklasse vereinfacht

--- WINF-Programmier-Projekt-2022/src/Backend/Hauptklasse.java
+++ WINF-Programmier-Projekt-2022/src/Backend/Hauptklasse.java
@@ -7,93 +7,98 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLSyntaxErrorException;
 import java.sql.Statement;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Hauptklasse {
 
-    static Connection conn;
-    static String url = "jdbc:mysql://3.69.96.96:3306/";
-    static String dbName = "db4";
-    static String driver = "com.mysql.cj.jdbc.Driver";
-    static String userName = "db4";
-    static String password = "!db4.hfts22?";
-    static UI frame;
-    public static final Logger log = Logger.getLogger(Hauptklasse.class.getName());
+	static Connection conn;
+	static String url = "jdbc:mysql://3.69.96.96:3306/";
+	static String dbName = "db4";
+	static String driver = "com.mysql.cj.jdbc.Driver";
+	static String userName = "db4";
+	static String password = "!db4.hfts22?";
+	static UI frame;
 
-    public static void main(String[] args) {
+	public static final Logger log = Logger.getLogger(Hauptklasse.class.getName());
 
-        try {
-            Class.forName(driver);
-            conn = DriverManager.getConnection(url + dbName, userName, password);
-            // nonsense Query but works, has no results
-            produktQuery("SELECT " + "Name, VRAM, Hersteller "
-                    + "FROM GRAFIKKARTEN WHERE HERSTELLER='ABCDEFG';", "Suche",new String[]{"Name", "VRAM",  "Hersteller" });
-            conn.close();
-        } catch (Exception e) {
-            log.log(Level.SEVERE, "Problem:", e);
-        }
-    }
+	public static void main(String[] args) {
 
-    /**
-     * Diese Methode führt SQL-Queries durch
-     * 
-     * @param p     Produkt welches gefiltert wird
-     * @param query String welcher Query enthält
-     * @throws SQLException
-     */
-    public static void produktQuery(String query, String oberflaeche, String[] tabelleneintrage)
-            throws SQLException {
-        log.info("Query: " + query + " in " + oberflaeche);
-        try {
-            Class.forName(driver);
-            conn = DriverManager.getConnection(url + dbName, userName, password);
-            String result = "";
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            int counter = 2;
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url + dbName, userName, password);
+			setup();
+		} catch (Exception e) {
+			log.log(Level.SEVERE, "Problem:", e);
+		}
+	}
 
-            // Tabelle Spalten benennen
-            for (int i = 0; i < tabelleneintrage.length; i++) {
-                result += tabelleneintrage[i] + "<<";
-            }
-            result += "Delete<<##";
-            result += " " + "<<" + " " + "<<" + " " + "<<" + "##";
+	public static void setup() {
+		try {
+			frame = new UI(nonsenseQuery());
+			frame.setVisible(true);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
-            // Alle Spalten holen
-            while (rs.next()) {
-                for (int i = 0; i < tabelleneintrage.length; i++) {
-                    result += rs.getString(tabelleneintrage[i]) + "<<";
-                }
-                result += "<<##";
-                counter++;
-            }
-            // Alle Daten in Array parsen
-            Object[][] ergebnis = new Object[counter][tabelleneintrage.length + 2];
-            ergebnis[0] = result.split("##")[0].split("<<");
-            for (int i = 0; i < counter; i++) {
-                ergebnis[i] = result.split("##")[i].split("<<");
-            }
-            // Wenn Frame nicht initialisiert wird er initialisiert.
-            if (frame == null) {
-                frame = new UI(ergebnis);
-                frame.setVisible(true);
-            } else {
-                if (oberflaeche.equals("Suche")) {
-                    frame.setSuchTable(ergebnis);
-                } else if (oberflaeche.equals("Einlagerung")) {
-                    frame.setEinlagerungTable(ergebnis);
-                }
+	public static Object[][] result(int counter, String result, int width) {
+		Object[][] ergebnis = new Object[counter][width];
+		ergebnis[0] = result.split("##")[0].split("<<");
+		for (int i = 0; i < counter; i++) {
+			ergebnis[i] = result.split("##")[i].split("<<");
+		}
+		System.out.print(Arrays.deepToString(ergebnis));
+		return ergebnis;
+	}
 
-            }
-            conn.close();
-        } catch (SQLSyntaxErrorException e) {
-            // nonsense Query but works
-            produktQuery("SELECT " + "Name, VRAM, Hersteller "
-                    + "FROM GRAFIKKARTEN WHERE HERSTELLER='ABCDEFG';", "Suche",new String[]{"Name", "VRAM",  "Hersteller" });
-            log.log(Level.SEVERE, "Problem:", e);
-        } catch (ClassNotFoundException e) {
-            log.log(Level.SEVERE, "Problem:", e);
-        }
-    }
+	public static void queryToUI(String query, String oberflaeche, String[] tabelleneintraege) {
+		try {
+			if (oberflaeche.equals("Suche")) {
+				frame.setSuchTable(queryToStringArray(query, tabelleneintraege));
+			} else if (oberflaeche.equals("Einlagerung")) {
+				frame.setEinlagerungTable(queryToStringArray(query, tabelleneintraege));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Object[][] queryToStringArray(String query, String[] tabelleneintrage) throws SQLException {
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			int counter = 2;
+			String result = "";
+
+			// Tabelle Spalten benennen
+			for (int i = 0; i < tabelleneintrage.length; i++) {
+				result += tabelleneintrage[i] + "<<";
+			}
+			result += "Delete<<##";
+			result += " " + "<<" + " " + "<<" + " " + "<<" + "##";
+
+			// Alle Spalten holen
+			while (rs.next()) {
+				for (int i = 0; i < tabelleneintrage.length; i++) {
+					result += rs.getString(tabelleneintrage[i]) + "<<";
+				}
+				result += "<<##";
+				counter++;
+			}
+			return result(counter, result, tabelleneintrage.length + 2);
+		} catch (SQLSyntaxErrorException e) {
+			nonsenseQuery();
+			log.log(Level.SEVERE, "Problem:", e);
+		}
+		return null;
+	}
+
+	public static Object[][] nonsenseQuery() throws SQLException {
+		return queryToStringArray(
+				"SELECT " + "Name, VRAM, Hersteller " + "FROM GRAFIKKARTEN WHERE HERSTELLER='ABCDEFG';",
+				new String[] { "Name", "VRAM", "Hersteller" });
+	}
+
 }
--- WINF-Programmier-Projekt-2022/src/UI/UI.java
+++ WINF-Programmier-Projekt-2022/src/UI/UI.java
@@ -28,296 +28,286 @@ import javax.swing.JTextField;
 
 public class UI extends JFrame {
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 1L;
-    private JPanel contentPane;
-    private JTabbedPane tabbedPane;
-    private JTable table;
-    private JPanel suche;
-    private JPanel panel_1;
-    private JPanel panel_2;
-    private JScrollPane scrollPane;
-    private JButton btnNewButton;
-    private JComboBox<String> dropdownSuche1;
-    private JComboBox<String> dropdownSuche2;
-    private JComboBox<String> dropdownSuche3;
-    private JComboBox<String> dropdownSuche4;
-    private JButton deleteSuche;
-    private JTextField textField;
-    private JTextField textField_1;
-    private JTextField textField_2;
-    private JButton okButton;
-    private JPanel einlagern;
-    private JPanel panel;
-    private JPanel panel_3;
-    private JComboBox<String> dropdownSuche1_1;
-    private JButton okButton_1;
-    private JScrollPane scrollPane_1;
-    private JButton btnNewButton_1;
-    private JTable einlagerungsTable;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTabbedPane tabbedPane;
+	private JTable table;
+	private JPanel suche;
+	private JPanel panel_1;
+	private JPanel panel_2;
+	private JScrollPane scrollPane;
+	private JButton btnNewButton;
+	private JComboBox<String> dropdownSuche1;
+	private JComboBox<String> dropdownSuche2;
+	private JComboBox<String> dropdownSuche3;
+	private JComboBox<String> dropdownSuche4;
+	private JButton deleteSuche;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JButton okButton;
+	private JPanel einlagern;
+	private JPanel panel;
+	private JPanel panel_3;
+	private JComboBox<String> dropdownSuche1_1;
+	private JButton okButton_1;
+	private JScrollPane scrollPane_1;
+	private JButton btnNewButton_1;
+	private JTable einlagerungsTable;
 
-    public UI() {
+	public UI() {
 
-    }
+	}
 
-    /**
-     * Create the frame.
-     * 
-     * @wbp.parser.constructor
-     */
-    public UI(Object[][] input) {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 987, 437);
-        contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(contentPane);
-        contentPane.setLayout(new GridLayout(0, 1, 0, 0));
-        tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-        contentPane.add(tabbedPane);
-        suche = new JPanel();
-        tabbedPane.addTab("Suche", null, suche, null);
-        suche.setLayout(new BorderLayout(0, 0));
-        panel_1 = new JPanel();
-        FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
-        flowLayout.setAlignment(FlowLayout.RIGHT);
-        suche.add(panel_1, BorderLayout.SOUTH);
-        deleteSuche = new JButton("delete");
-        deleteSuche.setToolTipText("put x in 'delete' column");
-        panel_1.add(deleteSuche);
-        btnNewButton = new JButton("Export");
-        panel_1.add(btnNewButton);
-        panel_2 = new JPanel();
-        suche.add(panel_2, BorderLayout.NORTH);
-        String[] arr = new String[] { "Grafikkarte", "Festplatte", "Hauptspeicher",
-                "Fertigprodukt", "CPU" };
-        dropdownSuche1 = new JComboBox<String>();
-        dropdownSuche1.addItem("");
-        for (String a : arr) {
-            dropdownSuche1.addItem(a);
-        }
-        dropdownSuche1.addActionListener(e -> updateDropdown2());
-        panel_2.add(dropdownSuche1);
-        dropdownSuche2 = new JComboBox<String>();
-        dropdownSuche2.addItem("");
-        dropdownSuche2.setSelectedIndex(0);
-        panel_2.add(dropdownSuche2);
-        textField = new JTextField();
-        panel_2.add(textField);
-        textField.setColumns(10);
-        dropdownSuche3 = new JComboBox<String>();
-        dropdownSuche3.addItem("");
-        dropdownSuche3.setSelectedIndex(0);
-        panel_2.add(dropdownSuche3);
-        textField_1 = new JTextField();
-        panel_2.add(textField_1);
-        textField_1.setColumns(10);
-        dropdownSuche4 = new JComboBox<String>();
-        dropdownSuche4.addItem("");
-        dropdownSuche4.setSelectedIndex(0);
-        panel_2.add(dropdownSuche4);
-        textField_2 = new JTextField();
-        panel_2.add(textField_2);
-        textField_2.setColumns(10);
-        okButton = new JButton("OK");
-        okButton.addActionListener(e -> querySuche());
-        panel_2.add(okButton);
-        scrollPane = new JScrollPane();
-        suche.add(scrollPane, BorderLayout.CENTER);
-        table = new JTable();
-        setSuchTable(input);
-        scrollPane.setViewportView(table);
+	/**
+	 * Create the frame.
+	 * 
+	 * @wbp.parser.constructor
+	 */
+	public UI(Object[][] input) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 987, 437);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(0, 1, 0, 0));
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		contentPane.add(tabbedPane);
+		suche = new JPanel();
+		tabbedPane.addTab("Suche", null, suche, null);                
 
-        einlagern = new JPanel();
-        tabbedPane.addTab("Einlagern", null, einlagern, null);
-        einlagern.setLayout(new BorderLayout(0, 0));
+		suche.setLayout(new BorderLayout(0, 0));
+		panel_1 = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
+		flowLayout.setAlignment(FlowLayout.RIGHT);
+		suche.add(panel_1, BorderLayout.SOUTH);
+		deleteSuche = new JButton("delete");
+		deleteSuche.setToolTipText("put x in 'delete' column");
+		panel_1.add(deleteSuche);
+		btnNewButton = new JButton("Export");
+		panel_1.add(btnNewButton);
+		panel_2 = new JPanel();
+		suche.add(panel_2, BorderLayout.NORTH);
+		String[] arr = new String[] { "Grafikkarte", "Festplatte", "Hauptspeicher", "Fertigprodukt", "CPU" };
+		dropdownSuche1 = new JComboBox<String>();
+		dropdownSuche1.addItem("");
+		for (String a : arr) {
+			dropdownSuche1.addItem(a);
+		}
+		dropdownSuche1.addActionListener(e -> updateDropdown2());
+		panel_2.add(dropdownSuche1);
+		dropdownSuche2 = new JComboBox<String>();
+		dropdownSuche2.addItem("");
+		dropdownSuche2.setSelectedIndex(0);
+		panel_2.add(dropdownSuche2);
+		textField = new JTextField();
+		panel_2.add(textField);
+		textField.setColumns(10);
+		dropdownSuche3 = new JComboBox<String>();
+		dropdownSuche3.addItem("");
+		dropdownSuche3.setSelectedIndex(0);
+		panel_2.add(dropdownSuche3);
+		textField_1 = new JTextField();
+		panel_2.add(textField_1);
+		textField_1.setColumns(10);
+		dropdownSuche4 = new JComboBox<String>();
+		dropdownSuche4.addItem("");
+		dropdownSuche4.setSelectedIndex(0);
+		panel_2.add(dropdownSuche4);
+		textField_2 = new JTextField();
+		panel_2.add(textField_2);
+		textField_2.setColumns(10);
+		okButton = new JButton("OK");
+		okButton.addActionListener(e -> querySuche());
+		panel_2.add(okButton);
+		scrollPane = new JScrollPane();
+		suche.add(scrollPane, BorderLayout.CENTER);
+		table = new JTable();
+		setSuchTable(input);
+		scrollPane.setViewportView(table);
 
-        panel = new JPanel();
-        FlowLayout flowLayout_2 = (FlowLayout) panel.getLayout();
-        flowLayout_2.setAlignment(FlowLayout.LEFT);
-        einlagern.add(panel, BorderLayout.NORTH);
+		einlagern = new JPanel();
+		tabbedPane.addTab("Einlagern", null, einlagern, null);
+		einlagern.setLayout(new BorderLayout(0, 0));
 
-        dropdownSuche1_1 = new JComboBox<String>();
-        for (String a : arr) {
-            dropdownSuche1_1.addItem(a);
-        }
-        panel.add(dropdownSuche1_1);
+		panel = new JPanel();
+		FlowLayout flowLayout_2 = (FlowLayout) panel.getLayout();
+		flowLayout_2.setAlignment(FlowLayout.LEFT);
+		einlagern.add(panel, BorderLayout.NORTH);
 
-        okButton_1 = new JButton("OK");
-        panel.add(okButton_1);
-        okButton_1.addActionListener(e -> queryEinlagern());
+		dropdownSuche1_1 = new JComboBox<String>();
+		for (String a : arr) {
+			dropdownSuche1_1.addItem(a);
+		}
+		panel.add(dropdownSuche1_1);
 
-        panel_3 = new JPanel();
-        FlowLayout flowLayout_1 = (FlowLayout) panel_3.getLayout();
-        flowLayout_1.setAlignment(FlowLayout.RIGHT);
-        einlagern.add(panel_3, BorderLayout.SOUTH);
+		okButton_1 = new JButton("OK");
+		panel.add(okButton_1);
+		okButton_1.addActionListener(e -> queryEinlagern());
 
-        btnNewButton_1 = new JButton("Lagerplaetze ausgeben");
-        btnNewButton_1.addActionListener(e -> queryEinlagern());
-        panel_3.add(btnNewButton_1);
+		panel_3 = new JPanel();
+		FlowLayout flowLayout_1 = (FlowLayout) panel_3.getLayout();
+		flowLayout_1.setAlignment(FlowLayout.RIGHT);
+		einlagern.add(panel_3, BorderLayout.SOUTH);
 
-        scrollPane_1 = new JScrollPane();
-        einlagern.add(scrollPane_1, BorderLayout.CENTER);
+		btnNewButton_1 = new JButton("Lagerplaetze ausgeben");
+		btnNewButton_1.addActionListener(e -> queryEinlagern());
+		panel_3.add(btnNewButton_1);
 
-        einlagerungsTable = new JTable();
-        scrollPane_1.setViewportView(einlagerungsTable);
-    }
+		scrollPane_1 = new JScrollPane();
+		einlagern.add(scrollPane_1, BorderLayout.CENTER);
 
-    /*
-     * Methode erzeugt Table neu.
-     */
-    public void setSuchTable(Object[][] input) {
-        Object[][] data = Arrays.copyOfRange(input, 1, input.length);
-        DefaultTableModel model = (DefaultTableModel) table.getModel();
-        model.setRowCount(0);
-        model = new DefaultTableModel(data, input[0]);
-        table.setModel(model);
-    }
+		einlagerungsTable = new JTable();
+		scrollPane_1.setViewportView(einlagerungsTable);
+	}
 
-    /*
-     * Methode welche eine Query basierend auf dem Zustand des UI erstellt.
-     */
-    public void querySuche() {
-        Produkt p;
-        switch (dropdownSuche1.getSelectedItem().toString()) {
-        case "Grafikkarte":
-            p = new Grafikkarte();
-            break;
-        case "Festplatte":
-            p = new Festplatte();
-            break;
-        case "Hauptspeicher":
-            p = new Hauptspeicher();
-            break;
-        case "CPU":
-            p = new CPU();
-            break;
-        default:
-            p = null;
-            break;
-        }
-        if (p != null) {
-            String sqlQuery = "SELECT ";
-            for (int i = 0; i < p.getTabelleneintraege().length; i++) {
-                sqlQuery += p.getTabelleneintraege()[i] + ", ";
-            }
-            sqlQuery = sqlQuery.substring(0, sqlQuery.length() - 2);
-            sqlQuery += " FROM " + p.produktTyp() + " ";
-            int added = 0;
-            if (!(dropdownSuche2.getSelectedItem().toString().length() <= 1)) {
-                sqlQuery += "WHERE " + dropdownSuche2.getSelectedItem() + "='"
-                        + textField.getText() + "' AND ";
-                added = 1;
-            }
-            if (!(dropdownSuche3.getSelectedItem().toString().length() <= 1)) {
-                if (added == 0) {
-                    sqlQuery += "WHERE ";
-                }
-                sqlQuery += dropdownSuche3.getSelectedItem() + "='" + textField_1.getText()
-                        + "' AND ";
-                added = 1;
+	/*
+	 * Methode erzeugt Table neu.
+	 */
+	public void setSuchTable(Object[][] input) {
+		Object[][] data = Arrays.copyOfRange(input, 1, input.length);
+		DefaultTableModel model = (DefaultTableModel) table.getModel();
+		model.setRowCount(0);
+		model = new DefaultTableModel(data, input[0]);
+		table.setModel(model);
+	}
 
-            }
-            if (!(dropdownSuche4.getSelectedItem().toString().length() <= 1)) {
-                if (added == 0) {
-                    sqlQuery += "WHERE ";
-                }
-                sqlQuery += dropdownSuche4.getSelectedItem() + "='" + textField_2.getText() + "'";
-                added = 0;
-            }
-            sqlQuery = sqlQuery.substring(0, sqlQuery.length() - 1 - added * 4) + ";";
-            sqlQuery = p == null ? null : sqlQuery;
-            try {
-                Hauptklasse.produktQuery(sqlQuery, "Suche", p.getTabelleneintraege());
-            } catch (SQLException e) {
-                Hauptklasse.log.log(Level.SEVERE,"Problem:", e);
-            }
-        }
-    }
+	/*
+	 * Methode welche eine Query basierend auf dem Zustand des UI erstellt.
+	 */
+	public void querySuche() {
+		Produkt p;
+		switch (dropdownSuche1.getSelectedItem().toString()) {
+		case "Grafikkarte":
+			p = new Grafikkarte();
+			break;
+		case "Festplatte":
+			p = new Festplatte();
+			break;
+		case "Hauptspeicher":
+			p = new Hauptspeicher();
+			break;
+		case "CPU":
+			p = new CPU();
+			break;
+		default:
+			p = null;
+			break;
+		}
+		if (p != null) {
+			String sqlQuery = "SELECT ";
+			for (int i = 0; i < p.getTabelleneintraege().length; i++) {
+				sqlQuery += p.getTabelleneintraege()[i] + ", ";
+			}
+			sqlQuery = sqlQuery.substring(0, sqlQuery.length() - 2);
+			sqlQuery += " FROM " + p.produktTyp() + " ";
+			int added = 0;
+			if (!(dropdownSuche2.getSelectedItem().toString().length() <= 1)) {
+				sqlQuery += "WHERE " + dropdownSuche2.getSelectedItem() + "='" + textField.getText() + "' AND ";
+				added = 1;
+			}
+			if (!(dropdownSuche3.getSelectedItem().toString().length() <= 1)) {
+				if (added == 0) {
+					sqlQuery += "WHERE ";
+				}
+				sqlQuery += dropdownSuche3.getSelectedItem() + "='" + textField_1.getText() + "' AND ";
+				added = 1;
 
-    /*
-     * Methode welche eine Query basierend auf dem Zustand des UI erstellt.
-     */
-    public void queryEinlagern() {
-        Produkt p;
-        switch (dropdownSuche1_1.getSelectedItem().toString()) {
-        case "Grafikkarte":
-            p = new Grafikkarte();
-            break;
-        case "Festplatte":
-            p = new Festplatte();
-            break;
-        case "Hauptspeicher":
-            p = new Hauptspeicher();
-            break;
-        case "CPU":
-            p = new CPU();
-            break;
-        default:
-            p = null;
-            break;
-        }
-        try {
-            Hauptklasse.produktQuery( "SELECT " + "Name, VRAM, Hersteller "
-                    + "FROM GRAFIKKARTEN WHERE HERSTELLER='ABCDEFG';", "Einlagerung",p.getTabelleneintraege());
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+			}
+			if (!(dropdownSuche4.getSelectedItem().toString().length() <= 1)) {
+				if (added == 0) {
+					sqlQuery += "WHERE ";
+				}
+				sqlQuery += dropdownSuche4.getSelectedItem() + "='" + textField_2.getText() + "'";
+				added = 0;
+			}
+			sqlQuery = sqlQuery.substring(0, sqlQuery.length() - 1 - added * 4) + ";";
+			sqlQuery = p == null ? null : sqlQuery;
+			Hauptklasse.queryToUI(sqlQuery, "Suche", p.getTabelleneintraege());
+		}
+	}
 
-    /*
-     * 
-     */
-    public void setEinlagerungTable(Object[][] input) {
-        Object[][] data = new Object[8][input[0].length];
-        for (int i = 0; i < input[0].length; i++) {
-            for (int a = 0; a < 8; a++) {
-                data[a][i] = "";
-            }
-        }
+	/*
+	 * Methode welche eine Query basierend auf dem Zustand des UI erstellt.
+	 */
+	public void queryEinlagern() {
+		Produkt p;
+		switch (dropdownSuche1_1.getSelectedItem().toString()) {
+		case "Grafikkarte":
+			p = new Grafikkarte();
+			break;
+		case "Festplatte":
+			p = new Festplatte();
+			break;
+		case "Hauptspeicher":
+			p = new Hauptspeicher();
+			break;
+		case "CPU":
+			p = new CPU();
+			break;
+		default:
+			p = null;
+			break;
+		}
+		Hauptklasse.queryToUI("SELECT " + "Name, VRAM, Hersteller " + "FROM GRAFIKKARTEN WHERE HERSTELLER='ABCDEFG';",
+				"Einlagerung", p.getTabelleneintraege());
+	}
 
-        DefaultTableModel model = (DefaultTableModel) table.getModel();
-        model.setRowCount(0);
-        model = new DefaultTableModel(data, input[0]);
-        einlagerungsTable.setModel(model);
-    }
+	/*
+	 * 
+	 */
+	public void setEinlagerungTable(Object[][] input) {
+		Object[][] data = new Object[8][input[0].length];
+		for (int i = 0; i < input[0].length; i++) {
+			for (int a = 0; a < 8; a++) {
+				data[a][i] = "";
+			}
+		}
 
-    /*
-     * Methode welche die JComboBoxen refresht
-     */
-    public void updateDropdown2() {
-        Produkt p;
-        dropdownSuche2.removeAllItems();
-        dropdownSuche3.removeAllItems();
-        dropdownSuche4.removeAllItems();
+		DefaultTableModel model = (DefaultTableModel) table.getModel();
+		model.setRowCount(0);
+		model = new DefaultTableModel(data, input[0]);
+		einlagerungsTable.setModel(model);
+	}
 
-        switch (dropdownSuche1.getSelectedItem().toString()) {
-        case "Grafikkarte":
-            p = new Grafikkarte();
-            break;
-        case "Festplatte":
-            p = new Festplatte();
-            break;
-        case "Hauptspeicher":
-            p = new Hauptspeicher();
-            break;
-        case "CPU":
-            p = new CPU();
-            break;
-        default:
-            p = null;
-            break;
-        }
-        dropdownSuche2.addItem("");
-        dropdownSuche3.addItem("");
-        dropdownSuche4.addItem("");
-        if (p != null) {
-            for (String a : p.getTabelleneintraege()) {
-                dropdownSuche2.addItem(a);
-                dropdownSuche3.addItem(a);
-                dropdownSuche4.addItem(a);
-            }
-        }
-    }
+	/*
+	 * Methode welche die JComboBoxen refresht
+	 */
+	public void updateDropdown2() {
+		Produkt p;
+		dropdownSuche2.removeAllItems();
+		dropdownSuche3.removeAllItems();
+		dropdownSuche4.removeAllItems();
+
+		switch (dropdownSuche1.getSelectedItem().toString()) {
+		case "Grafikkarte":
+			p = new Grafikkarte();
+			break;
+		case "Festplatte":
+			p = new Festplatte();
+			break;
+		case "Hauptspeicher":
+			p = new Hauptspeicher();
+			break;
+		case "CPU":
+			p = new CPU();
+			break;
+		default:
+			p = null;
+			break;
+		}
+		dropdownSuche2.addItem("");
+		dropdownSuche3.addItem("");
+		dropdownSuche4.addItem("");
+		if (p != null) {
+			for (String a : p.getTabelleneintraege()) {
+				dropdownSuche2.addItem(a);
+				dropdownSuche3.addItem(a);
+				dropdownSuche4.addItem(a);
+			}
+		}
+	}
 }
